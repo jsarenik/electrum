893f9302f293c819e2e13681ac1d981ffe508e89
Jan Sarenik <jajomojo@gmail.com>
2022-11-03 08:09:04 +0100

    More run scripts (text)

--- run_my.sh
+++ run_my.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 exec ./run_electrum \
+  "$@" \
   --oneserver \
   --server=bublina.eu.org:60002:s \
-  "$@"
 
 #  --server=192.168.1.118:60001:t \
---
+++ run_text.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+./run_my.sh gui -g text -w /home/nsm/.electrum/wallets/wallet_${1:-"ooo"}
